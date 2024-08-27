d2da0c0ee489c8d62bc721c7108bfd8628732092
Sharmili <rssharmili@gmail.com>
2024-08-27 21:15:28 +0530

    committed

--- React/sample/src/App.js
+++ React/sample/src/App.js
@@ -1,12 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Login from './Login';
+import Register from './Regiter';
+
 
 function App() {
  
   return (
     <div className="App">
       <Login/>
+      <Register/>
     </div>
   );
 }
---
+++ React/sample/src/Regiter.js
@@ -0,0 +1,30 @@
+import { useState } from "react";
+export default function Register() {
+    const[userList,setUserList]=useState([]);
+    async function getData() {
+        let res = await fetch("https://reqres.in/api/users?page=1",{method:"GET"});
+        let json = await res.json();
+        setUserList(json['data']);
+    }
+    
+    return(
+        <div>
+            <button onClick={()=>getData()}>Click</button>
+        
+        {
+            userList.map((obj,index)=>{
+                return(
+                    <div key={index}>
+                        <h1>{obj.first_name}</h1>
+                    </div>
+                )
+                
+            } )
+        }
+        </div>
+    )
+        
+}
+    
+        
+    
